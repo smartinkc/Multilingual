e0ef066112e93919b5e84647f21818cbd2911f7c
smartinkc <smartinkc@gmail.com>
2018-02-08 14:52:24 -0600

    Moved everything into the class, removed redcap_connect

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+	$data = @$_POST['data'];
+
+	if(isset($data) && $data != ''){
+		$data = json_decode($data, true);
+		
+		switch($data['todo']){
+			case 1:
+				\CMH\Multilingual\Multilingual::getTranslations($data);
+				break;
+			case 2:
+				\CMH\Multilingual\Multilingual::getAnswers($data);
+				break;
+			default:
+				exit;
+		}
+	}
+	elseif($_GET['todo'] == 2){
+		\CMH\Multilingual\Multilingual::exportData($_GET['pid'], $_GET['lang']);
+	}
+	else{
+		header("HTTP/1.0 404 Not Found");
+	}
+?>
